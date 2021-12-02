4683d16968b6c6db90a1bd3b68ce5f7354ee0bfc
Daisy Matus <daisymatus54@gmail.com>
2021-12-02 00:09:20 -0600

    Ejemplos de tipos de datos

---
+++ JAVA/DataTypes/src/Datos.java
@@ -0,0 +1,28 @@
+public class Datos {
+
+    public static void main(String[] args) {
+        byte edadErik = 15;
+        byte linesOfCode = 120; //límite (-128,127)
+
+        short s = 32767; //límite (-32767, 32767)
+
+        int cont = 1284;
+
+        long l = 1;
+
+        boolean b = true;
+
+        char letra = '1';
+
+        float num = 11.5f;
+
+        double d = 34.55;
+
+
+
+
+    }//main
+
+
+}// Class Datos
+
---
+++ JAVA/DataTypes/src/Wrappers.java
@@ -0,0 +1,24 @@
+public class Wrappers {
+
+    public static void main(String[] args) {
+        System.out.println("Byte max val " + Byte.MAX_VALUE);
+        System.out.println("Byte min val " + Byte.MIN_VALUE);
+
+        System.out.println("Short max val " + Short.MAX_VALUE);
+        System.out.println("Short min val " +Short.MIN_VALUE);
+
+        System.out.println("Integer max val " + Integer.MAX_VALUE);
+        System.out.println("Integer min val " + Integer.MIN_VALUE);
+
+        System.out.println("Long max val " + Long.MAX_VALUE);
+        System.out.println("Long min val " + Long.MIN_VALUE);
+
+        System.out.println("Float max val " + Float.MAX_VALUE);
+        System.out.println("Float min val " + Float.MIN_VALUE);
+
+        System.out.println("Double max val " + Double.MAX_VALUE);
+        System.out.println("Double min val " + Double.MIN_VALUE);
+
+    }
+
+}
